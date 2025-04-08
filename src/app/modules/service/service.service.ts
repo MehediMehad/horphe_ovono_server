3dec8ce885bf6a360172e0c5ca4e71e0c6564317
Mehedi Mehad <mdmehedihasanmehad@gmail.com>
2025-04-08 17:08:15 +0600

    create service

--- src/app/modules/service/service.service.ts
+++ src/app/modules/service/service.service.ts
@@ -1,10 +1,24 @@
-import { Service } from "@prisma/client";
+import { PrismaClient, Service } from "@prisma/client";
 
+
+const prisma = new PrismaClient()
 
 const createService = async (payload: Service) => {
 
+  const result = await prisma.service.create({
+    data: {
+      name: payload.name
+    },
+    select: {
+      id: true,
+      name: true,
+    }
+  })
 
-  return "hh"
+  return result
+
+
+
 };
 
 export const ServiceServices = {
